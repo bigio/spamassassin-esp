8a2db23498c34d826c223a4f0fb147eb4bd5b197
Giovanni Bechis <g.bechis@snb.it>
2021-02-11 12:31:36 +0100

    bump version

--- Esp.pm
+++ Esp.pm
@@ -43,7 +43,7 @@ use Mail::SpamAssassin::PerMsgStatus;
 use vars qw(@ISA);
 our @ISA = qw(Mail::SpamAssassin::Plugin);
 
-my $VERSION = 1.0;
+my $VERSION = 1.1;
 
 sub dbg { Mail::SpamAssassin::Plugin::dbg ("Esp: @_"); }
 
